3b5c3dffe90786eef12eb8e6635f7ac9a10f572b
Aman Kumar Singh <59767338+amanRathod@users.noreply.github.com>
2020-11-11 17:07:05 +0530

    Add files via upload

---
+++ majority element.cpp
@@ -0,0 +1,26 @@
+ class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+         int count=0;
+
+  int tech_school=0;
+
+  for(auto num:nums){
+
+   if(count==0)
+
+       tech_school=num;
+
+   if(tech_school==num)
+
+       count++;
+
+   else
+
+       count--;
+
+  }
+
+  return tech_school;
+  }
+};
