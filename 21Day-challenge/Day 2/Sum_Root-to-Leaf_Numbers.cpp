5e7d6e905533814826cb092ed58e6650722a568e
Aman Kumar Singh <59767338+amanRathod@users.noreply.github.com>
2021-01-05 10:51:41 +0530

    Update Sum_Root-to-Leaf_Numbers.cpp

--- 21Day-challenge/Day 2/Sum_Root-to-Leaf_Numbers.cpp
+++ 21Day-challenge/Day 2/Sum_Root-to-Leaf_Numbers.cpp
@@ -1,1 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 
+// LevelBase store value upto that level in a tree excluding present Node
+class Solution {
+public:
+    int sumNumbers(TreeNode* root) {
+        if(root == NULL)
+            return 0;
+      return SUM(root);
+    }
+  int SUM(TreeNode* root, int levelbase = 0){
+     if(root == NULL)
+        return 0;
+    if(root->left == NULL && root->right == NULL){
+    return levelbase + root->val;
+    }
+    
+    int presentbase = (levelbase + root->val)*10;
+    
+      int leftsum = SUM(root->left, presentbase);
+     int rightsum = SUM(root->right, presentbase);
+    
+    return leftsum + rightsum;
+   
+   
+  }
+};
