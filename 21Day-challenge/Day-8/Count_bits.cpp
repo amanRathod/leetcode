3dae0d9b0ff330019fff037484f0a881ce93face
Aman Kumar Singh <59767338+amanRathod@users.noreply.github.com>
2021-01-21 11:01:17 +0530

    Update Count_bits.cpp

--- 21Day-challenge/Day-8/Count_bits.cpp
+++ 21Day-challenge/Day-8/Count_bits.cpp
@@ -1,1 +1,15 @@
 
+class Solution {
+public:
+    vector<int> countBits(int num) {
+       
+        vector<int> res(num+1);
+        res[0] = 0;
+        
+        for(int i = 1; i<= num; ++i){
+            res[i] = res[i/2] + i%2; 
+        }
+        return res;
+        
+    }
+};
