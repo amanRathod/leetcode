b3fabe297565181dc732a3be4252a11b3fab098e
Aman Kumar Singh <59767338+amanRathod@users.noreply.github.com>
2021-02-02 11:17:29 +0530

    Update Palidromic_Substring.cpp

--- 21Day-challenge/Day-10/Palidromic_Substring.cpp
+++ 21Day-challenge/Day-10/Palidromic_Substring.cpp
@@ -1,1 +1,20 @@
 
+class Solution {
+public:
+    int count = 0;
+    void extendPalindrome(string s, int left, int right) {
+        while (left >=0 && right < s.length() && s[left]== s[right]) {
+            count++; left--; right++;
+        }
+    }
+    int countSubstrings(string s) {
+        if( s.length() == 0 )
+            return 0;
+        
+        for(int i = 0; i <= s.length()-1; ++i){
+           extendPalindrome(s, i, i); // odd length;
+            extendPalindrome(s, i, i + 1); // even length
+        }
+        return count;
+    }
+};
