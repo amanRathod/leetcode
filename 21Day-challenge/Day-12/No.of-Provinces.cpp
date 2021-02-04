40697f9bbb4e2b89c1dfdcc4f797f7cc62287673
Aman Kumar Singh <59767338+amanRathod@users.noreply.github.com>
2021-02-04 10:56:52 +0530

    Update No.of-Provinces.cpp

--- 21Day-challenge/Day-12/No.of-Provinces.cpp
+++ 21Day-challenge/Day-12/No.of-Provinces.cpp
@@ -1,1 +1,34 @@
 
+class Solution {
+public:
+    int find(int x, vector<int>& parn){
+        return parn[x] == x ? x:find(parn[x], parn);
+    }
+    int findCircleNum(vector<vector<int>>& isConnected) {
+          
+        int n = isConnected.size();
+        if(n== 0)
+            return 0;
+        vector<int> v(n);
+        int grp = n;
+        
+        for(int i = 0; i < n; ++i){
+            v[i] = i;
+        }
+        
+        for(int i= 0; i< n; ++i){
+            for(int j = i+1; j < n; ++j){
+                if(isConnected[i][j]){
+                    int n1 = find(i, v);
+                    int n2 = find(j, v);
+                    
+                    if(n1 != n2){
+                        v[n1] = n2;
+                        grp--;
+                    }
+            }
+        }
+        }
+            return grp;
+    }
+};
