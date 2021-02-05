184cdbf84271beb053e46288bc6f9bf6596dafbf
Aman Kumar Singh <59767338+amanRathod@users.noreply.github.com>
2021-02-05 12:06:41 +0530

    Update #594.cpp

--- 21Day-challenge/Day-13/#594.cpp
+++ 21Day-challenge/Day-13/#594.cpp
@@ -1,1 +1,18 @@
 
+class Solution {
+public:
+    int findLHS(vector<int>& nums) {
+        unordered_map<int,int> map;
+        
+        for(int i = 0; i < nums.size(); ++i){
+            map[nums[i]]++;
+        }
+        int maxi = 0;
+        for(auto it: map){
+            if(map.count(it.first-1) > 0){
+                maxi = max(maxi, it.second+map[it.first-1]);
+            }
+        }
+        return maxi;
+    }
+};
